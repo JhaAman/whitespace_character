788503130a469b79c67ad243c29ae88a0f2e9bf7
Joseph Yunis <yunjoey@rocketmail.com>
2021-02-28 16:32:02 -0500

    Made test react page that gets data from a public api and displays it on screen.

---
+++ frontend/reacttest/src/App.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function App() {
+  const [facts,setFact] = useState(null);
+  const apiURL = "https://api.chucknorris.io/jokes/random";
+  const fetchFact = async () => {
+    const response = await axios.get(apiURL);
+    setFact(response.data);
+    console.log(facts);
+  }
+
+  return (
+    <div className="App">
+      <h1>CHUCK NORRIS FACTS</h1>
+      <div className="facts">
+        {facts.value}
+      </div>
+      <div>
+        <button className="apifetchbutton" onClick={fetchFact}>Get New Fact</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
