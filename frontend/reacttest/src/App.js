5c38205f94a040e405edca68b96c879f6ae3c81b
lilyyangboston <65832486+lilyyangboston@users.noreply.github.com>
2021-03-02 16:27:08 -0500

    changes

--- frontend/reacttest/src/App.js
+++ frontend/reacttest/src/App.js
@@ -1,26 +1,39 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, { Component } from 'react';
+import Banner from 'react-js-banner';
+import './App.css';
+import logo from './logo.jpg';
+import GaugeChart from 'react-gauge-chart'
 
-function App() {
-  const [facts,setFact] = useState(null);
-  const apiURL = "https://api.chucknorris.io/jokes/random";
-  const fetchFact = async () => {
-    const response = await axios.get(apiURL);
-    setFact(response.data);
-    console.log(facts);
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bannerCss: { color: "#FFF", backgroundColor: "red", fontSize: 40 }
+    };
+
   }
-
-  return (
-    <div className="App">
-      <h1>CHUCK NORRIS FACTS</h1>
-      <div className="facts">
-        {facts.value}
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1 className="App-title">Group Goal Meter</h1>
+        <GaugeChart id="gauge-chart" 
+          nrOfLevels={100} 
+          color={["#EE3426"]} 
+          arcWidth={0.3} 
+          percent={0.37} 
+        />
+          <h3 className="App-title">Who's the Rockstar Employee of the Week? Scroll down below to find out!</h3>
+        </header>
+        <Banner title="John Doe" image={logo} imageClass="App-logo" css={this.state.banner2Css}/>
+        <Banner showBanner={true}>
+        </Banner>
       </div>
-      <div>
-        <button className="apifetchbutton" onClick={fetchFact}>Get New Fact</button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
+
