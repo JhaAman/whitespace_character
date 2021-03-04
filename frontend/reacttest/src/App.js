92f36257ae5ca9be6e697ff5f83798d3c8fbbb31
lilyyangboston <65832486+lilyyangboston@users.noreply.github.com>
2021-03-04 14:58:04 -0500

    merged

--- frontend/reacttest/src/App.js
+++ frontend/reacttest/src/App.js
@@ -9,13 +9,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      bannerCss: { color: "#FFF", backgroundColor: "red", fontSize: 40 }
+      bannerCss: { color: "#FFF", backgroundColor: "orange", fontSize: 40 }
     };
 
   }
   
   render() {
     return (
+      <div>
       <div className="App">
         <header className="App-header">
         <h1 className="App-title">Group Goal Meter</h1>
@@ -25,12 +26,33 @@ class App extends Component {
           arcWidth={0.3} 
           percent={0.37} 
         />
-          <h3 className="App-title">Who's the Rockstar Employee of the Week? Scroll down below to find out!</h3>
+          <h3 className="App-title">Who's the Rockstar Employee of the Week? Scroll down below!</h3>
         </header>
         <Banner title="John Doe" image={logo} imageClass="App-logo" css={this.state.bannerCss}/>
         <Banner showBanner={true}>
         </Banner>
       </div>
+        <div className='menu-box'>
+            <div className='team-logo-box'>
+                <h1 className='team-logo'>Team </h1>
+            </div>
+            <div className='menu-links-box'>
+                <h1 className='menu-link'>Homepage</h1>
+                <h1 className='menu-link'>My Recognitions</h1>
+                <h1 className='menu-link'>Sent Recognitions</h1>
+                <h1 className='menu-link'>Send Message</h1>
+                <h1 className='menu-link'>Send Recognition</h1>
+            </div>
+            <div className='bottom-menu-box'>
+                <div className='bottom-menu-button'>
+                    Settings
+                </div>
+                <div className='bottom-menu-button' style={{color: 'red'}}>
+                    Logout
+                </div>
+            </div>
+        </div>
+        </div>
     );
   }
 }
