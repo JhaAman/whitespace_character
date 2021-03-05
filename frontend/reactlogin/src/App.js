bdacf341d5f0d24569a9bc4d98cb9157c4ea5ef3
wmchristopher <79227197+wmchristopher@users.noreply.github.com>
2021-03-05 00:26:01 -0500

    trying again, now with understanding of git add
    lol I hope you like it after all that

---
+++ frontend/reactlogin/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>white space - login page</title>
+      </head>
+      <body>
+        <header>
+          <i>whitespaß</i>
+        </header>
+        <frontier>
+        </frontier>
+        <formbox>
+          <br></br><br></br>
+          <input type="text" placeholder="üsername" autofocus/><br></br><br></br>
+          <input type="password" placeholder="passwort"/>
+        </formbox>
+      </body>
+    </html>
+  );
+}
+
+export default App;
