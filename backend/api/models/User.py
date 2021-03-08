566867a1792672a09e6d54d715ccfffc16ec461c
duykhang09 <duykhang160600@gmail.com>
2021-03-08 11:09:11 +0700

    Edit basic Log-in API

--- backend/api/models/User.py
+++ backend/api/models/User.py
@@ -5,3 +5,5 @@ class User(models.Model):
     tid = models.CharField(max_length=8, default='00000000')
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length = 70, default='BlankSpace@gmail.com')
+    password = models.CharField(max_length=9, default= '000000000')
