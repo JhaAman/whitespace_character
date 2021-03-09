a410b5556dad3081998a86b6b88e9fa687c4b09f
garyszekely <67302008+garyszekely@users.noreply.github.com>
2021-03-09 13:21:51 -0500

    Added Toolbar to Homepage

--- frontend/react/src/Components/SideMenu/SideMenu.js
+++ frontend/react/src/Components/SideMenu/SideMenu.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SideMenu.css'
 
-function SideMenu() {
+function SideMenu({isOpen, setIsOpen}) {
     return (
         <div className='menu-box'>
             <div className='team-logo-box'>
+                <button onClick={() => setIsOpen(!isOpen)}>Close</button>
                 <h1 className='team-logo'>Team </h1>
             </div>
             <div className='menu-links-box'>
-                <h1 className='menu-link'>Homepage</h1>
-                <h1 className='menu-link'>My Recognitions</h1>
-                <h1 className='menu-link'>Sent Recognitions</h1>
-                <h1 className='menu-link'>Send Message</h1>
-                <h1 className='menu-link'>Send Recognition</h1>
+                <Link className='menu-link' to='/homepage'>Homepage</Link>
+                <Link className='menu-link'>My Recognitions</Link>
+                <Link className='menu-link'>Sent Recognitions</Link>
+                <Link className='menu-link'>Send Message</Link>
+                <Link className='menu-link'>Send Recognition</Link>
             </div>
             <div className='bottom-menu-box'>
                 <div className='bottom-menu-button'>
--- frontend/react/src/Pages/Homepage.js
+++ frontend/react/src/Pages/Homepage.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Banner from 'react-js-banner';
 import GaugeChart from 'react-gauge-chart';
 import logo from './logo.jpg';
-import SideMenu from './../Components/SideMenu/SideMenu.js';
+import SideMenu from '../Components/SideMenu/SideMenu.js';
+import Employee_Toolbar from '../Components/Toolbars/Employee_Toolbar.js';
+import './Homepage.css';
+import axios from 'axios';
 
 function Homepage() {
+
+    const [ isSideMenuOpen, setIsSideMenuOpen ] = useState(false);
+
     return (
         <div>
-            <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Group Goal Meter</h1>
-                    <GaugeChart id="gauge-chart" 
-                        nrOfLevels={100} 
-                        color={["#EE3426"]} 
-                        arcWidth={0.3} 
-                        percent={0.37} 
-                    />
-                    <h3 className="App-title">Who's the Rockstar Employee of the Week? Scroll down below!</h3>
-                </header>
-                <Banner title="John Doe" image={logo} imageClass="App-logo" css={{ color: "#FFF", backgroundColor: "orange", fontSize: 40 }} />
-                <Banner showBanner={true} />
+            <Employee_Toolbar isOpen={isSideMenuOpen} setIsOpen={setIsSideMenuOpen} />
+            {
+                isSideMenuOpen ? <SideMenu isOpen={isSideMenuOpen} setIsOpen={setIsSideMenuOpen} /> : undefined
+            }
+            <div className='main-content-box'>
+                <div style={{width: '30%', height: '100%', border: '1px solid yellow'}}>
+                </div>
+                <div style={{width: '40%', height: '100%', border: '1px solid green'}}>
+                    <header className="App-header">
+                        <h1 className="App-title">Group Goal Meter</h1>
+                        <GaugeChart id="gauge-chart" 
+                            nrOfLevels={100} 
+                            color={["#EE3426"]} 
+                            arcWidth={0.3} 
+                            percent={0.37} 
+                        />
+                        <h3 className="App-title">Who's the Rockstar Employee of the Week? Scroll down below!</h3>
+                    </header>
+                    <Banner title="John Doe" image={logo} imageClass="App-logo" css={{ color: "#FFF", backgroundColor: "orange", fontSize: 40 }} />
+                    <Banner showBanner={true} />
+                </div>
+                <div style={{width: '30%', height: '100%', border: '1px solid yellow'}}>
+                </div>
             </div>
-            <SideMenu />
         </div>
     );
 }
