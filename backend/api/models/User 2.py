d11b0d4e9df4e7a46b93320bb72860dda1f06b2a
lilyyangboston <65832486+lilyyangboston@users.noreply.github.com>
2021-03-09 16:51:26 -0500

    deleted some more duplicate files

--- backend/api/models/User 2.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    uid = models.CharField(max_length=8, default='00000000')
-    tid = models.CharField(max_length=8, default='00000000')
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
--- backend/api/models/Vote 3.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Vote(models.Model):
-    vid = models.CharField(max_length=8, default='00000000')
-    tags = models.CharField(max_length=120)
