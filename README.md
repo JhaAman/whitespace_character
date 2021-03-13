2fec2b7b3fd3d0ca1b3da185a68c5044dc6b52f8
Duy Pham <cunbidun@gmail.com>
2021-03-13 11:18:06 +0700

    Add swagger api docs

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 
 ## Backend
 
+There is a Swagger document at `http://localhost:8000/api/swagger/`
+
 ## How to run the APIs server
 1. run `docker-compose up --build` to build and run the backend server
 
 ## How to test
 1. run `./test.sh` to test the backend server
+
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
+from django.conf.urls import url
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from . import api
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Whitespace character APIs",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('dummy_api/', api.dummy_api, name='dummy_api'),
@@ -8,4 +21,5 @@ urlpatterns = [
     path('create_user/', api.create_user),
     path('create_users/', api.create_users),
     path('create_vote/', api.create_vote),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
