48c8f530a0d15f866d3aee816e0c2eb991027e65
lilyyangboston <65832486+lilyyangboston@users.noreply.github.com>
2021-03-17 03:06:24 -0400

    idk

---
+++ backend/api/urls 3.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import api
+
+urlpatterns = [
+    path('dummy_api/', api.dummy_api),
+    path('get_users/', api.get_users),
+    path('create_user/', api.create_user),
+    path('create_users/', api.create_users),
+    path('create_vote/', api.create_vote),
+]
