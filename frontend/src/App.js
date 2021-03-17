ccbf46c29e27d07761db530f7d73173e0ede92e4
Breadnbuns <68678244+Breadnbuns@users.noreply.github.com>
2021-03-17 02:35:37 -0400

    Recognitions v1
    First iteration  of recognitions.
    
    Still need formatting, photo, color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import React from 'react';
+import Recog from './Recognition';
 
 function App() {
   return (
-    <div>
-      Hello, fox
+    <div className = 'App'>
+      <h1>Recognitions and Such</h1>
+      <Recog name = "Kenny G" message = "Keeping it saxy."/>
+      <Recog name = "Black Betty" message = "Whoa, Black Betty (Bam-ba-lam)
+Whoa, Black Betty (Bam-ba-lam)
+Black Betty had a child (Bam-ba-lam)
+The damn thing gone wild (Bam-ba-lam)
+She said, I'm worryin' outta mind (Bam-ba-lam)
+The damn thing gone blind (Bam-ba-lam)
+I said oh, Black Betty (Bam-ba-lam)
+Whoa, Black Betty (Bam-ba-lam)
+Whoa, Black Betty (Bam-ba-lam)
+Whoa, Black Betty (Bam-ba-lam)
+She really gets me high (Bam-ba-lam)
+You know that's no lie (Bam-ba-lam)
+She's so rock steady (Bam-ba-lam)
+And she's always ready (Bam-ba-lam)
+Whoa, Black Betty (Bam-ba-lam)
+Whoa, Black Betty (Bam-ba-lam)
+Get it!"/> 
+      <Recog name = "Robot Rock" message = "Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock.
+      Robot rock. Robot rock. Robot rock.Robot rock. Robot rock. Robot rock.Robot rock. Robot rock. Robot rock.Robot rock. Robot rock. Robot rock. 
+      Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. 
+      Robot rock. Robot rock. Robot rock. Robot rock."/>
+
+      <Recog name = "Mississippi Queen" message = "If you know what I mean."/>
     </div>
   );
 }
---
+++ frontend/src/Recognition.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+
+function Recog(props) {
+    return(
+        <div className = "recog">
+            <h3>{props.name}</h3>
+            <h4></h4>
+            <p>{props.message}</p>
+        </div>
+    );
+}
+
+export default Recog;
