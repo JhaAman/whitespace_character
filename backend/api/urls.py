9ec5bcb46a5591bd658b722e1a6a5986550d14d7
duykhang09 <duykhang160600@gmail.com>
2021-03-22 11:18:47 +0700

    Test

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,31 @@
 from django.urls import path
-from . import api
+from django.conf.urls import url
+from rest_framework import permissions
+#from drf_yasg.views import get_schema_view
+#from drf_yasg import openapi
+
+import api.services.user as apiUser
+import api.services.recognition as apiRecognition
+import api.services.profile as apiProfile
+import api.services.auth as apiAuth
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Whitespace character APIs",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('dummy_api/', api.dummy_api),
-    path('get_users/', api.get_users),
-    path('create_user/', api.create_user),
-    path('create_users/', api.create_users),
-    path('create_vote/', api.create_vote),
-    path('log_in/', api.log_in),
-    path('parsing_values/', api.parsing_values),
+    # path('dummy_api/', api.dummy_api),
+    path('get_users/', apiUser.get_users),
+    path('create_user/', apiUser.create_user),
+    path('create_users/', apiUser.create_users),
+    path('create_vote/', apiRecognition.create_vote),
+    path('get_profile/', apiProfile.get_profile),
+    path('log_in/', apiAuth.log_in),
+    path('log_out/', apiAuth.log_out)
+    #url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
