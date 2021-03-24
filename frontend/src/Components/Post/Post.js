aff40c507741eb8c9fedb74c4b3bfe9a82f1731d
Reuben <rcphilip2@gmail.com>
2021-03-24 11:40:57 -0400

    Adding post and universal
    Added post.js and universal.css

--- frontend/src/Components/Post/Post.js
+++ frontend/src/Components/Post/Post.js
@@ -1,32 +1,29 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+
 import './Post.css'
 
 function Post() {
-  return (
+    return (
     <div style = {{width: "300px"}}>
-    
-
       <input
         style={{ width:"300px" }}
         type = "text"
         placeholder = "name"
       ></input>
       <br></br>
-
       <input 
         style={{ width:"300px" }}
         type = "text"
         placeholder = "subject"
       ></input>
       <br></br>
-
       <input 
         style={{ width:"300px", height: "100px" }}
         type = "text"
         placeholder = "body"
       ></input>
       <br></br>
-
       <div style = {{
         justifyContent: 'space-evenly',
         flex: 1,
