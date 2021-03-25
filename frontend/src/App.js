178f4a7f560d4eae529f6ddc5e96a3b3ed3d5537
Breadnbuns <68678244+Breadnbuns@users.noreply.github.com>
2021-03-25 12:23:52 -0400

    Git
    Made git work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,27 +7,8 @@ function App() {
     <div className = 'App'>
       <h1>Recognitions and Such</h1>
       <Recog name = "Kenny G" message = "Keeping it light."/>
-      <Recog name = "Black Betty" message = "Whoa, Black Betty (Bam-ba-lam)
-Whoa, Black Betty (Bam-ba-lam)
-Black Betty had a child (Bam-ba-lam)
-The damn thing gone wild (Bam-ba-lam)
-She said, I'm worryin' outta mind (Bam-ba-lam)
-The damn thing gone blind (Bam-ba-lam)
-I said oh, Black Betty (Bam-ba-lam)
-Whoa, Black Betty (Bam-ba-lam)
-Whoa, Black Betty (Bam-ba-lam)
-Whoa, Black Betty (Bam-ba-lam)
-She really gets me high (Bam-ba-lam)
-You know that's no lie (Bam-ba-lam)
-She's so rock steady (Bam-ba-lam)
-And she's always ready (Bam-ba-lam)
-Whoa, Black Betty (Bam-ba-lam)
-Whoa, Black Betty (Bam-ba-lam)
-Get it!"/> 
-      <Recog name = "Robot Rock" message = "Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock.
-      Robot rock. Robot rock. Robot rock.Robot rock. Robot rock. Robot rock.Robot rock. Robot rock. Robot rock.Robot rock. Robot rock. Robot rock. 
-      Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. Robot rock. 
-      Robot rock. Robot rock. Robot rock. Robot rock."/>
+      <Recog name = "Black Betty" message = "please"/>
+      <Recog name = "Robot Rock" message = "shoot shoot"/>
 
       <Recog name = "Mississippi Queen" message = "If you know what I mean."/>
     </div>
