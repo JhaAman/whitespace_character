483a1da577aef5fcd274762b8c68f1b83e6baf34
Joseph Yunis <yunjoey@rocketmail.com>
2021-04-04 00:19:59 -0400

    Made notifications in a popup button

--- frontend/notifications/src/App.js
+++ frontend/notifications/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 //import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import DropdownItem from 'react-bootstrap/DropdownItem';
+import Dropdown from 'react-bootstrap/Dropdown';
+//import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import {faBell} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+
 
 function Notification(props){
   return(
+    <DropdownItem>
     <div className={props.new+"-notification"}>
         <Row>
           <Col>{props.type}</Col>
@@ -27,6 +36,7 @@ function Notification(props){
         </Row>
       
     </div>
+    </DropdownItem>
     
   );
 }
@@ -35,30 +45,28 @@ function App() {
   let notifications = [];
   return (
     <div className="App">
-      <Navbar bg="light" expand="sm">
-        <Navbar.Brand href="#home">whitespaß</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-        
-        <NavDropdown title={"📢"+notifications.length} id="notificationsdropdown" >
-          <Notification message="You got a new recognition from Gary" type="Recognition" time="5 hours ago" new="yes"/>
-          <hr/>
+      
+
+
+      
+      <DropdownButton variant="light" title={<FontAwesomeIcon icon={faBell}/>} >
+      <Notification message="You got a new recognition from Gary" type="Recognition" time="5 hours ago" new="yes"/>
+          <Dropdown.Divider/>
           <Notification message="You got a new recognition from Sally" type="Recognition" time="8 hours ago" new="yes"/>
-          <hr/>
+          <Dropdown.Divider/>
           <Notification message="You received a new badge" type="Badge" time="12 hours ago" new="no"/>
-          <hr/>
+          <Dropdown.Divider/>
           <Notification message="Steve has been declared the Rockstar of the Month" type="Global" time="Yesterday" new="no"/>
-        </NavDropdown>
+      </DropdownButton>
 
 
-        <NavDropdown title="Garry Stevenson" id="profiledropdown" className="ml-auto">
-          <Nav.Link href="#Profile">Profile</Nav.Link>
 
-          <Nav.Link href="#Settings">Settings</Nav.Link>
 
-          <Nav.Link href="#Logout">Log Out</Nav.Link>
-        </NavDropdown>
-      </Navbar>
-      Hello world
+
+
+
+
+
     </div>
     
   );
