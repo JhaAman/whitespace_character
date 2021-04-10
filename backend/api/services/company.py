f063be4968344fb2138ec8f9ea21978c38ca4ead
k2nt <tuankhai2k@gmail.com>
2021-04-10 17:07:03 +0700

    added company/get endpoint

--- backend/api/services/company.py
+++ backend/api/services/company.py
@@ -16,9 +16,13 @@ from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
 from rest_framework import status
 
+from api.db.models import \
+    Company as Comp
+
 from api.db.serializers import \
     CompanySerializer as CompSrl, \
-    ApiResponseSerializer as ApiRespSrl
+    ApiResponseSerializer as ApiRespSrl, \
+    CidFormSerializer as CidFormSrl
 
 
 @api_view(["POST"])
@@ -62,3 +66,86 @@ def create(request):
                         'trace': e.args[0]
                     }).data,
                 status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def get(request):
+    try:
+        # Serialize incoming request
+        requestSrl = CidFormSrl(data=request.data)
+
+        # If data fields are invalid, return error report
+        if not requestSrl.is_valid():
+            return \
+                Response(
+                    data=
+                        ApiRespSrl({
+                            'status': status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            'msg': "Cannot create Company object: Invalid field",
+                            'trace': requestSrl.errors
+                        }).data,
+                    status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        # Get validated ata
+        requestDict = requestSrl.validated_data
+        # Get Company object
+        compObj = Comp.objects.get(cid=requestDict['cid'])
+        # Deserialize object
+        compDict = CompSrl(compObj).data
+        # Return success report
+        return \
+            Response(
+                data=
+                    ApiRespSrl({
+                        'status': status.HTTP_200_OK,
+                        'msg': "Fetched Company object",
+                        'datat': compDict
+                    }).data,
+                status=status.HTTP_200_OK)
+
+    except ValueError as e:
+        # If Exception occurs, return error report
+        return \
+            Response(
+                data=
+                    ApiRespSrl({
+                        'status': status.HTTP_400_BAD_REQUEST,
+                        'msg': "Cannot fetch Company object: Exception ocurred",
+                        'trace': e.args[0]
+                    }).data,
+                status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['UPDATE'])
+# def update(request):
+#     # Serialize incoming request
+#     requestSrl = CidFormSrl(data=request.data)
+
+#     # If data fields are invalid, return error report
+#     if not requestSrl.is_valid():
+#         return \
+#             Response(
+#                 data=
+#                     ApiRespSrl({
+#                         'status': status.HTTP_422_UNPROCESSABLE_ENTITY,
+#                         'msg': "Cannot create Company object: Invalid field",
+#                         'trace': requestSrl.errors
+#                     }).data,
+#                 status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+#     # Get validated ata
+#     requestDict = requestSrl.validated_data
+#     # Get Company object
+#     compObj = Comp.objects.get(cid=requestDict['cid'])
+#     # Deserialize object
+#     compDict = CompSrl(compObj).data
+#     # Return success report
+#     return \
+#         Response(
+#             data=
+#                 ApiRespSrl({
+#                     'status': status.HTTP_200_OK,
+#                     'msg': "Fetched Company object",
+#                     'datat': compDict
+#                 }).data,
+#             status=status.HTTP_200_OK)
