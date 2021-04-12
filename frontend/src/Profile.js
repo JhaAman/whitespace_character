07d0d59747c4582a74e10efd9dd0365c9939dc9e
Joseph Yunis <yunjoey@rocketmail.com>
2021-04-12 07:34:11 -0400

    started work on the profile settings page added 75 profile pic choices

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -4,9 +4,11 @@ import Image from 'react-bootstrap/Image';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import {AuthenticationContext} from './AuthContext.js';
+import images from './Images.js';
 
 function Networkprofile(props) {
   return (
@@ -24,6 +26,15 @@ function Award(props) {
     </Col>
   )
 }
+
+function ProfilePictureChoice(props) {
+  return (
+    <Col className="profilepicturechoice">
+      <Image src={props.src} style={{height:"100px",width:"100px"}} roundedCircle/>
+    </Col>
+  )
+}
+
 // function DevColors(){
 //   return(
 // <div>
@@ -35,7 +46,7 @@ function Award(props) {
 //       </div>
 //   )
 // }
-let profileAPI = "http://localhost:8000/api/get_profile/";
+let profileAPI = "http://localhost:8000/api/user/get/";
 
 
 function Profile() {
@@ -45,9 +56,14 @@ function Profile() {
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(0);
   const { userid } = useParams();
-
+  const value = useContext(AuthenticationContext);
+  const [pictures,setPictures] = useState([]);
+  
   useEffect(() => {
+    MakePictureComponent();
     getData();
+    
+    //console.log(pictures);
     // eslint-disable-next-line
   }, []);
 
@@ -55,7 +71,10 @@ function Profile() {
     axios.get(profileAPI, {
       params: {
         uid: userid
-      }
+      },
+      headers:{
+        Authorization:"Bearer "+value.authenticationState.token
+      },
     })
       .then(function (res) {
         if (res.status === 200) {
@@ -81,16 +100,75 @@ function Profile() {
           setPeople(p);
         }
       })
-      .catch(error => console.error(error));
+      .catch(error => {
+        console.error(error);
+        console.error("debug");
+        
+        let sample = {
+          "status": "200",
+          "msg": "Fetched requested user",
+          "data": {
+              "tid": "36590184",
+              "uid": "52793423",
+              "first_name": "Brett",
+              "last_name": "Harrison",
+              "email": "bh@apple.com",
+              "job_title": "Project Manager",
+              "badges": [],
+              "network": [
+                  "99723579",
+                  "52793423",
+                  "23747122"
+              ],
+              "values_scores": {
+                  "caring": 0,
+                  "succint": 0,
+                  "leadership": 0
+              },
+              "profile_picture": null,
+              "user_role": "mng",
+              "date_created": "2021-04-11 05:43 UTC",
+              "password": "fruits"
+          },
+          "trace": null
+      };
+        setData(sample);
+        setLoading(false);
+
+        let a = sample.data.badges;
+          let b = []
+          for (let i = 0; i < a.length; i++) {
+
+            //console.log(a[i]);
+            b.push(<Award key={i} award={a[i]} />);
+          }
+          setAwards(b);
+          let dpeople = sample.data.network;
+          let p = [];
+          for (let i = 0; i < dpeople.length; i++) {
+            //console.log(dpeople[i]);
+            let name = dpeople[i].first_name + " " + dpeople[i].last_name;
+            p.push(<Networkprofile key={i} name={name} picture={profilepic} />)
+          }
+          setPeople(p);
+
+      });
   }
 
+  function MakePictureComponent(){
+    let r = []
+    for(let a in images){
+      r.push(<ProfilePictureChoice src={images[a]} alt="profile picture"/>)
+    }
+    setPictures(r);
+  }
 
   if (loading) {
     return <div className="App">Loading</div>
   }
 
 
-  console.log(data);
+  //console.log(data);
 
 
   return (
@@ -110,6 +188,8 @@ function Profile() {
           <button className="button topbutton" onClick={() => setPage(0)}>Badges</button>
           <Col xs={1}></Col>
           <button className="button topbutton" onClick={() => setPage(1)}>Network</button>
+          <Col xs={1}></Col>
+          <button className="button topbutton" onClick={() => setPage(2)}>Settings</button>
         </div>
         <br />
       </div>
@@ -123,13 +203,27 @@ function Profile() {
 
           </Row>
         </div>
-      ) : (
+      ) : page===1 ?(
         <div className="contentpanel">
           <br />
           {people}
 
         </div>
-      )}
+      ) :
+      <div className="contentpanel">
+        <br/>
+        <div>
+          Profile Settings
+          <div className="optionbox">
+            Select a profile picture
+            <div className="picturescroll">
+              <Row>{pictures}</Row>
+            </div>
+          </div>
+        </div>
+
+
+      </div>}
 
 
       {/*<DevColors/>**/}
