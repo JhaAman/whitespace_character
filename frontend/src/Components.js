39836dbaac534b00cfdb052ee3de872dddd69e48
lilyyangboston <65832486+lilyyangboston@users.noreply.github.com>
2021-04-17 22:16:55 -0400

    added recognition profile pic icons + resized recognition report box + attempted links to user profiles

--- frontend/src/Components.js
+++ frontend/src/Components.js
@@ -3,14 +3,11 @@ import { Link } from 'react-router-dom';
 import './App.css';
 import Popup from 'reactjs-popup';
 import Image from 'react-bootstrap/Image';
-<<<<<<< Updated upstream
-=======
 
 import NotificationButton from './Notification';
 
 import ProfilePicture from './pics/arnold.jpg'
 
->>>>>>> Stashed changes
 
 /* Now storing objects here, so they have access to all the right directories and CSS and suchlike. If this upset anyone, let me know. ~ Christopher */
 /* To add an object here, though, one must add the line “// eslint-disable-next-line” to escape the wrath of the garbage fairy. */
@@ -26,21 +23,12 @@ function Pass() {
 }
 
 function Recognition(props){
-<<<<<<< Updated upstream
-  return (
-      <div className="recognition rounded">
-          <div className="topline">
-          <Image src={props.recognizerpicture} className="rounded-circle" width="30px" height="auto"></Image>
-              <i><b>{props.recognizer}</b> cherishes <b>{props.recipient}</b></i>
-          <Image src={props.recipientpicture} className="rounded-circle" width="30px" height="auto"></Image>
-=======
     return (
         <div className="recognition rounded">
             <div className="topline">
             <Image src={props.recognizerpicture} className="rounded-circle" width="30px" height="auto"></Image>
             <Link className='link' to={props.recognizerlink}><i><b>{props.recognizer}</b></i></Link> cherishes <Link className='link' to={props.recipientlink}><i><b>{props.recipient}</b></i></Link>
             <Image src={props.recipientpicture} className="rounded-circle" width="30px" height="auto"></Image>
->>>>>>> Stashed changes
           </div>
           <br></br>
           <div>
@@ -92,5 +80,27 @@ function TopMenu({isOpen, setIsOpen}) {
     );
 }
 export { TopMenu }
+
+// eslint-disable-next-line
+/* This will be part of my April 4 week CSS cleanup ~ Christopher */
+function Rockstar({value, firstName, lastName}) {
+    return (
+        <div style={{height: '150px', width: '90%', border: '2px solid black', marginBottom: '5px'}}>
+            <div style={{width: '100%', height: '20%', display: 'flex', justifyContent: 'center', alignItems: 'center', fontWeight: 'bold', borderBottom: '2px solid black'}}>
+                <h1 style={{fontSize: '12pt', fontWeight: 'bold', margin: 0}}>ROCKSTAR OF THE MONTH: {value}</h1>
+            </div>
+            <div style={{width: '100%', height: '80%', display: 'flex', flexDirection: 'row'}}>
+                <div style={{width: '50%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <img src={ProfilePicture} style={{width: '100px', height: '100px', borderRadius: '50%', border: '2px solid black'}}/>
+                </div>
+                <div style={{width: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly'}}>
+                    <h2 style={{fontSize: '15pt', margin: 0}}>{firstName} {lastName}</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { Rockstar };
 
 export default Pass;
--- frontend/src/EmployeeHomepage.js
+++ frontend/src/EmployeeHomepage.js
@@ -1,20 +1,4 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import React from 'react';
-=======
->>>>>>> Stashed changes
-import profilepic from './pics/arnold.jpg';
-import profilepic2 from './pics/regina.png';
-import { Recognition, TopMenu } from './Components.js';
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import axios from 'axios'
-import React, { useEffect, useState, useContext } from 'react';
-import { useParams } from 'react-router-dom';
-import { Profile } from './Profile.js';
-<<<<<<< Updated upstream
-=======
+
 import { Recognition, TopMenu, Rockstar } from './Components.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
@@ -26,40 +10,16 @@ import axios from 'axios'
 import React, { useEffect, useContext } from 'react';
 import { Profile } from './Profile.js';
 import { AuthenticationContext } from './AuthContext.js';
->>>>>>> Stashed changes
-=======
-import { AuthenticationContext } from './AuthContext.js';
->>>>>>> Stashed changes
 
 let profileAPI = "http://localhost:8000/api/get_profile/";
 
 function EmployeeHomepage() {
-<<<<<<< Updated upstream
-    const { userid } = useParams();
-    const [ setData ] = useState();
-=======
-
-    const value = useContext(AuthenticationContext);
->>>>>>> Stashed changes
 
     const value = useContext(AuthenticationContext);
 
       useEffect(() => {
         getData();
       }, []);
-<<<<<<< Updated upstream
-    
-<<<<<<< Updated upstream
-      let getData = () => {
-        axios.get(profileAPI, {
-          params: {
-            uid: userid
-          }
-        })
-        .then(function (res) {
-            setData(res);
-        })}
-=======
 
       const getData = () => {
         axios.get("http://localhost:8000/api/swagger/" + value.uID,
@@ -79,73 +39,25 @@ function EmployeeHomepage() {
     }
 
     const rockstarsArray = [{value: 'Communications', firstName: 'Gary', lastName: 'Szekely'}, {value: 'Hard-Working', firstName: 'Reuben', lastName: 'Philip'}, {value: 'Inclusive', firstName: 'Khang', lastName: 'Nguyen'}]
->>>>>>> Stashed changes
-=======
-    //   let getData = () => {
-    //     axios.get(profileAPI, {
-    //       params: {
-    //         uid: userid
-    //       }
-    //     })
-    //     .then(function (res) {
-    //         setData(res);
-    //     })}
-
-        function getData(){
-            axios.post("http://localhost:8000/u/",
-                    {uID:57940164},
-                    {
-                        headers:{
-                            Authorization:"Bearer "+value.uID
-                        }
-                    }
-                    )
-                    .then(function(response){
-                        console.log(value.uID);
-                    });
-        }
->>>>>>> Stashed changes
 
     return (
        
         <div className="app">
             <TopMenu/>
-<<<<<<< Updated upstream
-            <div class="body">
-                <div class="row">
-                    <div className='left-column'>
-                        <Recognition recognizerpicture={profilepic2} recipientpicture={profilepic} recipient="Reginald" recognizer="Gwen" message="Everyone is raving about your work on pantelic aristism."/>
-                        <Recognition recognizerpicture={profilepic} recipientpicture={profilepic2} recipient="Edith" recognizer="Lancelot" message="I love your ideative reification!"/>
-                        <Recognition recognizerpicture={profilepic2} recipientpicture={profilepic} recipient="Millard" recognizer="Eleanor" message="2τ/10 withstandingship at the weekly witenagemot"/>
-=======
             <div className="body">
                 <div className="row">
                 <div className='left-column'>
                         <Recognition recognizerpicture={profilepic2} recognizerlink='/u/'getData recipientpicture={profilepic} recipientlink='/u/'getData recipient="Reginald" recognizer="Gwen" message="Everyone is raving about your work on pantelic aristism."/>
                         <Recognition recognizerpicture={profilepic} recognizerlink='/u/'getData recipientpicture={profilepic2} recipientlink='/u/'getData recipient="Edith" recognizer="Lancelot" message="I love your ideative reification!"/>
                         <Recognition recognizerpicture={profilepic2} recognizerlink='/u/'getData recipientpicture={profilepic} recipientlink='/u/'getData recipient="Millard" recognizer="Eleanor" message="2τ/10 withstandingship at the weekly witenagemot"/>
->>>>>>> Stashed changes
                     </div>
                     <div className='right-column'>
                         <div className='infobox rounded'>
-<<<<<<< Updated upstream
-                            <br></br>
-                            <b><i><div style={{fontSize:30}}>This month’s hero:</div></i></b>
-                            <img src={profilepic} className="rounded-circle" width="150px" height="auto" alt="Smiling guy"></img>
-                            <br></br><br></br>
-                            <Link className='link' to='/u/'getData>Charles Martel</Link>
-                            <br></br><br></br>
-                            [other statistics can go here]
-                            <br></br>
-                            <br></br>
-                            <br></br>
-=======
                             {
                                 rockstarsArray.map((e) => {
                                     return (<Link className='link' to='/u/'getData><Rockstar value={e.value} firstName={e.firstName} lastName={e.lastName}/></Link>);
                                 })
                             }
->>>>>>> Stashed changes
                         </div>
                     </div>
                 </div>
