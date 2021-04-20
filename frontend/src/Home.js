a8b03e26db5e9bf74b259376e58f550cbb4305f2
lilyyangboston <65832486+lilyyangboston@users.noreply.github.com>
2021-04-20 13:19:57 -0400

    idk what change this is

---
+++ frontend/src/Home.js
@@ -0,0 +1,64 @@
+import { Recognition, TopMenu, Rockstar } from './Components.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import ManagerComp from './ManagerComponent.js';
+import profilepic from './pics/arnold.jpg';
+import profilepic2 from './pics/regina.png';
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+import React, { useEffect, useContext } from 'react';
+import { Profile } from './Profile.js';
+import { AuthenticationContext } from './AuthContext.js';
+ 
+function EmployeeHomepage() {
+ 
+   const value = useContext(AuthenticationContext);
+ 
+     useEffect(() => {
+       const getData = () => {
+           axios.post("http://127.0.0.1:8000/api/create_user/", {
+               params: {
+                 uID: value.uID
+               }
+             })
+           .then(function(res){
+               console.log(res);
+           })
+           .catch(error => {
+               console.error(error)
+           });
+       }
+       getData();
+     }, []);
+ 
+   const rockstarsArray = [{value: 'Communications', firstName: 'Gary', lastName: 'Szekely'}, {value: 'Hard-Working', firstName: 'Reuben', lastName: 'Philip'}, {value: 'Inclusive', firstName: 'Khang', lastName: 'Nguyen'}]
+ 
+   return (
+     
+       <div className="app">
+           <TopMenu/>
+           <div className="body">
+               <div className="row">
+               <div className='left-column'>
+                       <Recognition recognizerpicture={profilepic2} recognizerlink='/u/'useEffect recipientpicture={profilepic} recipientlink='/u/'useEffect recipient="Reginald" recognizer="Gwen" message="Everyone is raving about your work on pantelic aristism."/>
+                       <Recognition recognizerpicture={profilepic} recognizerlink='/u/'useEffect recipientpicture={profilepic2} recipientlink='/u/'useEffect recipient="Edith" recognizer="Lancelot" message="I love your ideative reification!"/>
+                       <Recognition recognizerpicture={profilepic2} recognizerlink='/u/'useEffect recipientpicture={profilepic} recipientlink='/u/'useEffect recipient="Millard" recognizer="Eleanor" message="2τ/10 withstandingship at the weekly witenagemot"/>
+                   </div>
+                   <div className='right-column'>
+                       <div className='infobox rounded'>
+                           {
+                               rockstarsArray.map((e) => {
+                                   return (<Link className='link' to='/u/'useEffect><Rockstar value={e.value} firstName={e.firstName} lastName={e.lastName}/></Link>);
+                               })
+                           }
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+      
+   
+   );
+}
+ 
+export default EmployeeHomepage;
