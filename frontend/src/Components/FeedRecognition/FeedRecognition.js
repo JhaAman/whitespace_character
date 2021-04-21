1ea79a4c97a4547481486781ebc3c444a0f8f966
garyszekely <67302008+garyszekely@users.noreply.github.com>
2021-04-20 22:09:54 -0400

    Finished recognition

--- frontend/src/Components/FeedRecognition/FeedRecognition.js
+++ frontend/src/Components/FeedRecognition/FeedRecognition.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import React, { useState, useContext, useEffect} from 'react';
 import { AuthContext } from './../../AuthContext.js';
-import Arnold from '../../pics/arnold.jpg'
+import ProfilePicture from '../../pics/arnold.jpg'
 import './FeedRecognition.css'
 
 function FeedRecognition({uidFrom, uidTo, comment}) {
     const context = useContext(AuthContext)
     const [ fromName, setFromName ] = useState("");
     const [ toName, setToName ] = useState("");
+    const [ profilePicture, setProfilePicture ] = useState("")
 
     const getNames = () => {
         axios.get("http://127.0.0.1:8000/api/user/get_name/", {
@@ -37,6 +38,7 @@ function FeedRecognition({uidFrom, uidTo, comment}) {
             }
         }).then((res) => {
             console.log(res);
+            setProfilePicture('http://localhost:8000' + res.data[uidFrom])
         }).catch((err) => {
             console.log(err);
         })
@@ -48,7 +50,7 @@ function FeedRecognition({uidFrom, uidTo, comment}) {
         <div className='main-container'>
             <div className='left-container'>
                 <div className='profile-container'>
-                    <img src={Arnold} style={{width: '125px', height: '125px', borderRadius: '50%', border: '2px solid black'}} />
+                    <img src={(profilePicture === "Nothing" || profilePicture === "") ? ProfilePicture : profilePicture} style={{width: '125px', height: '125px', borderRadius: '50%', border: '2px solid black'}} />
                 </div>
                 <div style={{height: '50%', width: '100%'}} />
             </div>
