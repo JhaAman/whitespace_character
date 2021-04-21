8f9efcfceb038225849cbaf39714e44f884de15d
k2nt <tuankhai2k@gmail.com>
2021-04-22 06:10:06 +0700

    managed merge conflicts with main

---
+++ frontend/src/Components/Rockstar/Rockstar.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import React, { useState, useContext, useEffect } from 'react';
+import ProfilePicture from './../../pics/arnold.jpg'
+import { AuthContext } from './../../AuthContext.js';
+
+function Rockstar({value, uid, name, profilePicture}) {
+    const context = useContext(AuthContext);
+
+    return (
+        <div style={{height: '150px', width: '90%', border: '2px solid black', marginBottom: '5px', backgroundColor: 'grey'}}>
+            <div style={{width: '100%', height: '20%', display: 'flex', justifyContent: 'center', alignItems: 'center', fontWeight: 'bold', borderBottom: '2px solid black'}}>
+                <h1 style={{fontSize: '12pt', fontWeight: 'bold', margin: 0}}>ROCKSTAR OF THE MONTH: {value}</h1>
+            </div>
+            <div style={{width: '100%', height: '80%', display: 'flex', flexDirection: 'row'}}>
+                <div style={{width: '50%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <img src={(profilePicture === "Nothing" || profilePicture === "") ? ProfilePicture : profilePicture} style={{width: '100px', height: '100px', borderRadius: '50%', border: '2px solid black'}}/>
+                </div>
+                <div style={{width: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly'}}>
+                    <h2 style={{fontSize: '15pt', margin: 0}}>{name}</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Rockstar;
