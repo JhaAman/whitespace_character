27379feaa6704f6f01a2bf2d774ee3142ef30128
garyszekely <67302008+garyszekely@users.noreply.github.com>
2021-04-22 21:23:04 -0400

    Fixed AuthContext errors and linked rockstars and profiles

--- frontend/src/Components/FeedRecognition/FeedRecognition.js
+++ frontend/src/Components/FeedRecognition/FeedRecognition.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useState, useContext, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import { AuthContext } from './../../AuthContext.js';
 import ProfilePicture from '../../pics/arnold.jpg'
 import './FeedRecognition.css'
